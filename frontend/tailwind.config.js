b982a542e09ce50c76fe46be7afb62f5f430281e
Malindu Dominic Inosh Atukorala <dominic.malindu@gmail.com>
2026-03-26 18:30:15 +0530

    Minor tweaks

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
-    theme: { extend: {} },
-    plugins: [],
-  }
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: { extend: {} },
+  plugins: [],
+}
